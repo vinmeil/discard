af332a08d525e22cf2e937d02c2381f58dd1c046
Ryan Dyson Darmawan <dysonryan6@gmail.com>
2024-06-20 22:51:19 +0800

    fix: added nextConfig modules in hopes itll fix something skull

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    reactStrictMode: true,
+    pageExtensions: ['ts', 'tsx'], // Include TypeScript files as pages
+  }
+  
+module.exports = nextConfig
