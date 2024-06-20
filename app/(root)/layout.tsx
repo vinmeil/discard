fe73f5f4bcb85ad306052e78fe23b1bf564b8222
Ryan Dyson Darmawan <dysonryan6@gmail.com>
2024-06-20 22:34:18 +0800

    fix: fixed test commit not rendering with child error

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,14 +1,15 @@
 import Page from "./page";
 import { Header } from "./navigation/Header";
 import { Inter } from "next/font/google";
+import { type ReactNode } from "react";
 
 const inter = Inter({subsets: ['latin']});
 
-export default function Layout() {
+export default function Layout({children}: {children: ReactNode}) {
     return (
         <div className={inter.className}>
             <Header />
-            <Page />
+            {children}
         </div>
     );
 }
