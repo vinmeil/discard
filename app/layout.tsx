e9af50bff947baf9a17046794733f649872e00f9
Vincent <vincent.wesley.liem@gmail.com>
2024-06-20 23:04:17 +0800

    add layout.tsx to /app

---
+++ app/layout.tsx
@@ -0,0 +1,12 @@
+import { Inter } from "next/font/google";
+import { type ReactNode } from "react";
+
+const inter = Inter({subsets: ['latin']});
+
+export default function Layout({children}: {children: ReactNode}) {
+    return (
+        <div className={inter.className}>
+            {children}
+        </div>
+    );
+}
