abb032b80936f50a7c8e2d54daf13a03d3d3da89
RYAN DARMAWAN <ryan.dyson@wemakeapp.net>
2024-07-17 13:18:08 +0800

    fix: fixed tailwindcss not working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,23 @@
+import { type Metadata } from "next";
 import { Inter } from "next/font/google";
 import { type ReactNode } from "react";
 import { Header } from "./_components/Header";
+import "./globals.css";
 
 const inter = Inter({subsets: ['latin']});
 
+export const metadata: Metadata = {
+    title: "Discard",
+    description: "Discard is a voice and chatting platform",
+}
+
 export default function RootLayout({children}: {children: ReactNode}) {
     return (
-        <html className={inter.className}>
-            <body>
+        <html lang="en">
+            <body className={inter.className}>
                 <Header />
                 {children}
             </body>
         </html>
     );
-}
+} 
---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Page() {
+    return (
+        <main>
+            <div>
+                <h1 className="bg-gray-900">This Will be the landing page</h1>
+            </div>
+        </main>
+    )
+}
